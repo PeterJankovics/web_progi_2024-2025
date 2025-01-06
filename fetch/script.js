5f698ab0d737a4b9a74d229de346c12454f51e8e
PeterJankovics <jankovics.peter.21@ady-nagyatad.hugit config --global user.email jankovics.peter.21@ady-nagyatad.hugit config --global user.email jankovics.peter.21@ady-nagyatad.hu>
2025-01-06 21:40:20 +0100

    asdsad

---
+++ fetch/script.js
@@ -0,0 +1,25 @@
+const newUser = {
+    name: 'Maria',
+    job: 'Teacher'
+};
+
+fetch('https://reqres.in/api/users/55', {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newUser)
+}).then(res => {
+    if(!res.ok){
+        console.log('Problem');
+        return;
+    }
+
+    return res.json();
+})
+.then(data => {
+    console.log('Success');
+})
+.catch(error => {
+    console.log(error);
+});
